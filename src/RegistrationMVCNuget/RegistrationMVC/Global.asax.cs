47f004aae65114ab7f5d27952dc0fda75749a84c
Alfonso9 <alfonso09.lr@gmail.com>
2017-05-07 23:46:48 -0500

    Starting project.

---
+++ src/RegistrationMVCNuget/RegistrationMVC/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using RegistrationMVC.App_Start;
+using RegistrationMVC.Infraestructure.Ninject;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace RegistrationMVC
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            /*Inyector de dependencias*/
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
+        }
+    }
+}
